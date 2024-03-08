5a8adae36df717311eb3a1219f5a97587acecaf1
Marcelo Santos <rocha.mer21@gmail.com>
2024-03-08 14:33:04 -0300

    feat: sqlite create db, and automigrate

--- config/sqlite.go
+++ config/sqlite.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"os"
 	"com.github/marcelosanto/gopportunities/schemas"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -8,8 +9,27 @@ import (
 
 func InitSqlLite(path string) (*gorm.DB, error) {
 	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
 
-	db, err := gorm.Open(sqlite.Open("./db/main.db"), &gorm.Config{})
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating...")
+
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			logger.Errorf("sqlite mkdir error: %v", err)
+			return nil, err
+		}
+
+		file, err := os.Create(dbPath)
+		if err != nil {
+			logger.Errorf("sqlite create error: %v", err)
+			return nil, err
+		}
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
 	
 	if err != nil {
 		logger.Errorf("sqlite opening error: %v", err)
